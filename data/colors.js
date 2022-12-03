c9ca57f5b8760cbce50d2ac839fe0f333382f220
elaounijassim <114288992+elaounijassim@users.noreply.github.com>
2022-12-03 15:36:45 +0100

    Add files via upload

---
+++ data/colors.js
@@ -0,0 +1,15 @@
+define(function() {
+return [
+	{"loc":[33.98, -6.85], "title":"CHU Ibn Sina"},
+	{"loc":[33.97, -6.86], "title":"HUI Cheikh Zaid"},
+	{"loc":[33.57, -7.62], "title":"CHU Ibn Rochd"},
+	{"loc":[33.55, -7.66], "title":"HU Cheikh Khalifa"},
+	{"loc":[35.68, -5.93], "title":"CHU Tanger"},
+	{"loc":[31.63, -8], "title":"CHU Mohammed VI"},
+	{"loc":[31.59, -8], "title":"HUP Marrakech"},	
+	{"loc":[30.40, -9.53], "title":"CHU Agadir"},
+	{"loc":[27.13, -13.14], "title":"CHU Lâayoune"},
+	{"loc":[34, -4.96], "title":"CHU Hassan II"},
+	{"loc":[34.65, -1.91], "title":"CHU Mohammed V"}
+];
+});
